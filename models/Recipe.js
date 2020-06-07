316a8d98ec74cf0bd1ec0d828cbf62cdc0f09479
koneone2002 <katieo3507@gmail.com>
2020-06-06 20:43:24 -0700

    contacts model and get contacts rout added

---
+++ models/Recipe.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const RecipeSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  ingredients: {
+    type: String
+  },
+  originalURL: {
+    type: String
+  },
+  type: {
+    type: String,
+    default: 'personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('recipe', RecipeSchema);
