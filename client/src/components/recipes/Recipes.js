a8df2dc46153690777273f5b3d905e1599e6f62e
koneone2002 <katieo3507@gmail.com>
2020-06-30 20:23:26 -0700

    added css transition to both recipe and contact page

--- client/src/components/recipes/Recipes.js
+++ client/src/components/recipes/Recipes.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import RecipeContext from '../../context/recipe/recipeContext';
 import RecipeItem from './RecipeItem';
 
@@ -11,9 +12,19 @@ const Recipes = () => {
   }
   return (
     <Fragment>
-      {filtered !== null
-        ? filtered.map(recipe => <RecipeItem recipe={recipe} key={recipe.id} />)
-        : recipes.map(recipe => <RecipeItem recipe={recipe} key={recipe.id} />)}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map(recipe => (
+              <CSSTransition key={recipe.id} timeout={500} classNames='item'>
+                <RecipeItem recipe={recipe} />
+              </CSSTransition>
+            ))
+          : recipes.map(recipe => (
+              <CSSTransition key={recipe.id} timeout={500} classNames='item'>
+                <RecipeItem recipe={recipe} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
